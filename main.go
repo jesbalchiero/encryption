537a7bb11c810f75c383afaabc3e3763471c1cb5
jesbalchiero <62852809+jesbalchiero@users.noreply.github.com>
2024-05-11 07:43:59 -0300

    fix(main): encrypt and decrypt

--- main.go
+++ main.go
@@ -19,15 +19,23 @@ func main() {
 
 }
 
-func hashLetterFn(key int, letter string) (result string) {
+func hashLetterFnOriginal(key int, letter string) (result string) {
 	runes := []rune(letter)
 	lastLetterKey := string(runes[len(letter)-key : len(letter)])
-	leftOverLetter := string(runes[0 : len(letter)-key])
+	leftOverLetters := string(runes[0 : len(letter)-key])
 
-	return fmt.Sprintf(`%s%s`, lastLetterKey, leftOverLetter)
+	return fmt.Sprintf(`%s%s`, lastLetterKey, leftOverLetters)
 }
 
-func encrypt(key int, plainText string) (result string) {
+func hashLetterFn(key int, letter string) (result string) {
+	runes := []rune(letter)
+	lastLetters := string(runes[len(letter)-key:])     // Gets the last letters based on the key
+	leftOverLetters := string(runes[:len(letter)-key]) // Gets the remaining letters
+
+	return fmt.Sprintf("%s%s", lastLetters, leftOverLetters)
+}
+
+func encryptOriginal(key int, plainText string) (result string) {
 	hashLetter := hashLetterFn(key, originalLetter)
 	var hashedString = ""
 
@@ -48,7 +56,23 @@ func encrypt(key int, plainText string) (result string) {
 	return hashedString
 }
 
-func decrypt(key int, encryptedText string) (result string) {
+func encrypt(key int, plainText string) (result string) {
+	var encrypted string
+	for _, char := range plainText {
+		if char >= 'A' && char <= 'Z' {
+			shifted := ((int(char) - 'A' + key) % 26) + 'A'
+			encrypted += string(shifted)
+		} else if char >= 'a' && char <= 'z' {
+			shifted := ((int(char) - 'a' + key) % 26) + 'a'
+			encrypted += string(shifted)
+		} else {
+			encrypted += string(char)
+		}
+	}
+	return encrypted
+}
+
+func decryptOriginal(key int, encryptedText string) (result string) {
 	hashLetter := hashLetterFn(key, originalLetter)
 	var hashedString = ""
 
@@ -67,4 +91,20 @@ func decrypt(key int, encryptedText string) (result string) {
 
 	strings.Map(findOne, encryptedText)
 	return hashedString
+}
+
+func decrypt(key int, encryptedText string) (result string) {
+	var decrypted string
+	for _, char := range encryptedText {
+		if char >= 'A' && char <= 'Z' {
+			shifted := ((int(char) - 'A' - key + 26) % 26) + 'A'
+			decrypted += string(shifted)
+		} else if char >= 'a' && char <= 'z' {
+			shifted := ((int(char) - 'a' - key + 26) % 26) + 'a'
+			decrypted += string(shifted)
+		} else {
+			decrypted += string(char)
+		}
+	}
+	return decrypted
 }
