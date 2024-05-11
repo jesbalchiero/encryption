15ed48e6684ff4ecb8b2d5d65c29b6a5584989b2
jesbalchiero <62852809+jesbalchiero@users.noreply.github.com>
2024-05-11 07:20:20 -0300

    feat(encrypt): add function to encrypt text

--- main.go
+++ main.go
@@ -1,1 +1,43 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+const originalLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func main() {
+	plainText := "Aleatory"
+	fmt.Println("Plain Text", plainText)
+
+	encrypted := encrypt(5, plainText)
+	fmt.Println("Encrypted text", encrypted)
+}
+
+func hashLetterFn(key int, letter string) (result string) {
+	runes := []rune(letter)
+	lastLetterKey := string(runes[len(letter)-key : len(letter)])
+	leftOverLetter := string(runes[0 : len(letter)-key])
+
+	return fmt.Sprintf(`%s%s`, lastLetterKey, leftOverLetter)
+}
+
+func encrypt(key int, plainText string) (result string) {
+	hashLetter := hashLetterFn(key, originalLetter)
+	var hashedString = ""
+
+	findOne := func(r rune) rune {
+		pos := strings.Index(originalLetter, string([]rune{r}))
+
+		if pos != -1 {
+			letterPosition := (pos + len(originalLetter)) % len(originalLetter)
+			hashedString = hashedString + string(hashLetter[letterPosition])
+		}
+
+		return r
+	}
+
+	strings.Map(findOne, plainText)
+	return hashedString
+}
